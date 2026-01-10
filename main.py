7f3bac4221a413c7703007bbfa74d7b00c581735
Mario Coloma <marioxcolomar@gmail.com>
2026-01-11 00:16:52 +0100

    Add messages types and verbose flag.

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+import argparse
 import os
 
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -10,17 +12,25 @@ if api_key is None:
 
 client = genai.Client(api_key=api_key)
 
-contents = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+
+parser = argparse.ArgumentParser(description="Chatbot")
+parser.add_argument("user_prompt", type=str, help="User prompt")
+parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+args = parser.parse_args()
+
+messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+
 response = client.models.generate_content(
     model="gemini-2.5-flash",
-    contents=contents,
+    contents=messages,
 )
 
 if response.usage_metadata is None:
     raise RuntimeError("usage error when calling generate_content")
 
-print(f"User prompt: {contents}")
-print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+if args.verbose:
+    print(f"User prompt: {args.user_prompt}")
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 print("Response:")
 print(response.text)
